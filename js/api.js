a3c16d65a65d2fa8177fdcca3ff376a147e871c8
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-19 18:24:09 -0500

    Commented axios function

--- js/api.js
+++ js/api.js
@@ -16,3 +16,22 @@ export default function getMusic(findMusic) {
       console.error(err);
     });
 }
+
+// import axios from "axios";
+
+// const URL = "https://deezerdevs-deezer.p.rapidapi.com/search";
+
+// export default async function getDataApi(findMusic) {
+//   try {
+//     const response = await axios.get(URL, {
+//       params: { q: `${findMusic}` },
+//       headers: {
+//         "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+//         "x-rapidapi-key": "118e940fd1mshae5c7ec4c61d02fp17f896jsn5361688cb6f2",
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
