b6d2245e30b7b56aba6a3bc2af051259a4dfb7fb
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-19 20:10:47 -0500

    Change of nameSong dinamic from headerList

--- js/components/music.js
+++ js/components/music.js
@@ -1,13 +1,21 @@
 import {
   $album,
+  $nameSong,
   $random,
   $back,
   $play,
   $next,
   $repeat,
-  playList
+  playList,
 } from "../utils/const.js";
 
 export default function listen(item = playList[0]) {
+  let audio = new Audio(item.preview);
+  audio.autoplay = false;
+  $play.addEventListener("click", function () {
+    audio.paused ? audio.play() : audio.pause();
+  });
   $album.style.backgroundImage = `url(${item.album.cover_big})`;
+  $nameSong.innerHTML = item.title_short
 }
+
