b5d5cfc1b95c695d80de7abaed3ac546f5487eeb
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-19 19:10:27 -0500

    Item of playList with an action

---
+++ js/components/item.js
@@ -0,0 +1,39 @@
+export default function showPlayList(list, action) {
+  let $list = document.querySelector(".list");
+  list.map((item, index) => {
+    let $item = document.createElement("BUTTON");
+    $item.classList.add("item");
+    $item.classList.add("btn");
+
+    let $pos = document.createElement("DIV");
+    $pos.classList.add("pos");
+    let numPos = document.createTextNode(index + 1);
+    $pos.appendChild(numPos);
+
+    let $name = document.createElement("DIV");
+    $name.classList.add("name");
+    let nameSong = document.createTextNode(item.title_short);
+    $name.appendChild(nameSong);
+
+    let $snippet = document.createElement("DIV");
+    $snippet.classList.add("snippet");
+    let $duration = document.createElement("SPAN");
+    $duration.classList.add("duration");
+    let time = document.createTextNode(
+      `${parseInt(item.duration / 60)}:${
+        item.duration % 60 < 10
+          ? "0" + (item.duration % 60)
+          : item.duration % 60
+      }`
+    );
+    $duration.appendChild(time);
+    $snippet.appendChild($duration);
+
+    $item.addEventListener("click", () => action(item));
+
+    $item.appendChild($pos);
+    $item.appendChild($name);
+    $item.appendChild($snippet);
+    $list.appendChild($item);
+  });
+}
