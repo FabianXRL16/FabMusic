af06eb2cb4c5dae4c975242ff9496ea8ce0dbbfa
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-23 15:16:35 -0500

    Incomplete song list shuffle function

--- js/app.js
+++ js/app.js
@@ -4,7 +4,16 @@ import song from "./components/item.js";
 import listenMusic from "./components/music.js";
 
 song(playList, listenMusic);
-listenMusic(0, false)
+listenMusic(0, false);
+
+document.querySelector("#random").addEventListener("click", function () {
+  let padre = document.querySelector(".list");
+  let item = document.querySelectorAll(".item");
+  item.forEach((i) => padre.removeChild(i));
+  let newPlayList = playList.sort(() => Math.random() - 0.5);
+  song(newPlayList, listenMusic);
+  console.log(newPlayList);
+});
 
 // import search from "./api.js";
 
