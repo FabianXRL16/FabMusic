e5c2f210925351c7634bbd11eab7963592c8e080
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-09 02:54:50 -0500

    Config of firebase

---
+++ js/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlMUbA9VCiSGK80d1zEjzoy9NMgxdN9GU",
+  authDomain: "fabmusic-e86b0.firebaseapp.com",
+  projectId: "fabmusic-e86b0",
+  storageBucket: "fabmusic-e86b0.appspot.com",
+  messagingSenderId: "913254939528",
+  appId: "1:913254939528:web:b2600ae56b5ed4877a183d",
+  measurementId: "G-XT4ZEWTZV3",
+};
